b4d840307758d76d5333a29613ab6a4e1eaa9bb4
Yami <renard.ryan74@gmail.com>
2023-12-21 22:23:56 +0100

    finished implementation basic Greeks

--- black_scholes.go
+++ black_scholes.go
@@ -13,16 +13,19 @@ type Option struct {
 	V float64 // Volatility
 }
 
+// Compute the Standard Normal Cummulative Density Function give a value x.
 func standardNormalCDF(x float64) float64 {
 	return 0.5 * (1 + math.Erf(x/math.Sqrt(2)))
 }
 
+// Compute the Gaussian Probability Density Function given a value x, mean, and standard deviation. Return a float64.
 func gaussianPDF(x, mean, stddev float64) float64 {
 	const invSqrt2Pi = 0.3989422804014327 // 1 / sqrt(2 * pi)
 	a := (x - mean) / stddev
 	return invSqrt2Pi / stddev * math.Exp(-0.5*a*a)
 }
 
+// CallPrice compute the call price of an European option using the Black Schole Formula. Return a float64 and an error object.
 func (o *Option) CallPrice() (float64, error) {
 	if o.K == 0 {
 		return 0, errors.New("strike price must be greater than 0")
@@ -32,6 +35,7 @@ func (o *Option) CallPrice() (float64, error) {
 	return o.S*standardNormalCDF(d1) - o.K*math.Exp(-o.R*o.T)*standardNormalCDF(d2), nil
 }
 
+// PutPrice compute the put price of an European option using the Black Schole Formula. Return a float64 and an error object.
 func (o *Option) PutPrice() (float64, error) {
 	if o.K == 0 {
 		return 0, errors.New("strike price must be greater than 0")
@@ -41,6 +45,7 @@ func (o *Option) PutPrice() (float64, error) {
 	return o.K*math.Exp(-o.R*o.T)*standardNormalCDF(-d2) - o.S*standardNormalCDF(-d1), nil
 }
 
+// CallDelta compute the delta of an European call option using the Black Schole Formula. Return a float64 and an error object.
 func (o *Option) CallDelta() (float64, error) {
 	if o.K == 0 {
 		return 0, errors.New("strike price must be greater than 0")
@@ -49,6 +54,7 @@ func (o *Option) CallDelta() (float64, error) {
 	return standardNormalCDF(d1), nil
 }
 
+// PutDelta compute the delta of an European put option using the Black Schole Formula. Return a float64 and an error object.
 func (o *Option) PutDelta() (float64, error) {
 	if o.K == 0 {
 		return 0, errors.New("strike price must be greater than 0")
@@ -57,6 +63,7 @@ func (o *Option) PutDelta() (float64, error) {
 	return standardNormalCDF(d1) - 1, nil
 }
 
+// Gamma compute the gamma of an European option using the Black Schole Formula. Return a float64 and an error object
 func (o *Option) Gamma() (float64, error) {
 	if o.K == 0 || o.S == 0 || o.T == 0 {
 		return 0, errors.New("strike price, spot price and time to maturity must be greater than 0")
@@ -65,19 +72,59 @@ func (o *Option) Gamma() (float64, error) {
 	return gaussianPDF(d1, 0, 1) / (o.S * o.V * math.Sqrt(o.T)), nil
 }
 
+// Vega compute the vega of an European option using the Black Schole Formula. Return a float64 and an error object
 func (o *Option) Vega() (float64, error) {
 	if o.K == 0 {
 		return 0, errors.New("strike price must be greater than 0")
 	}
 	d1 := (math.Log(o.S/o.K) + (o.R+0.5*math.Pow(o.V, 2))*o.T) / (o.V * math.Sqrt(o.T))
-	return o.S * standardNormalCDF(d1) * math.Sqrt(o.T), nil
+	return o.S * gaussianPDF(d1, 0,1) * math.Sqrt(o.T) / 100, nil
 }
 
+// CallTheta compute the theta of an European call option using the Black Schole Formula. Return a float64 and an error object
 func (o *Option) CallTheta() (float64, error) {
 	if o.K == 0 {
 		return 0, errors.New("strike price must be greater than 0")
 	}
 	d1 := (math.Log(o.S/o.K) + (o.R+0.5*math.Pow(o.V, 2))*o.T) / (o.V * math.Sqrt(o.T))
 	d2 := d1 - o.V*math.Sqrt(o.T)
-	return (-o.S*o.V*standardNormalCDF(d1))/(2*math.Sqrt(o.T)) - o.R*o.K*math.Exp(-o.R*o.T)*standardNormalCDF(d2), nil
+	part1 := -o.S*o.V*gaussianPDF(d1, 0,1) / (2*math.Sqrt(o.T))
+	part2 := -o.R*o.K*math.Exp(-o.R*o.T)*standardNormalCDF(d2)
+	theta := part1 + part2
+	return theta / 365, nil
+}
+
+// PutTheta compute the theta of an European put option using the Black Schole Formula. Return a float64 and an error object
+func (o *Option) PutTheta() (float64, error) {
+	if o.K == 0 {
+		return 0, errors.New("strike price must be greater than 0")
+	}
+	d1 := (math.Log(o.S/o.K) + (o.R+0.5*math.Pow(o.V, 2))*o.T) / (o.V * math.Sqrt(o.T))
+	d2 := d1 - o.V*math.Sqrt(o.T)
+	part1 := -o.S*o.V*gaussianPDF(d1, 0,1) / (2*math.Sqrt(o.T))
+	part2 :=o.R*o.K*math.Exp(-o.R*o.T)*standardNormalCDF(-d2)
+	theta := part1 + part2
+	return theta /365, nil
+}
+
+// CallRho compute the rho of an European call option using the Black Schole Formula. Return a float64 and an error object
+func (o *Option) CallRho() (float64, error) {
+	if o.K == 0 {
+		return 0, errors.New("strike price must be greater than 0")
+	}
+	d1 := (math.Log(o.S/o.K) + (o.R+0.5*math.Pow(o.V, 2))*o.T) / (o.V * math.Sqrt(o.T))
+	d2 := d1 - o.V*math.Sqrt(o.T)
+	rho := o.K * o.T * (math.Exp(-o.R * o.T)) * standardNormalCDF(d2)
+	return rho / 100, nil
+}
+
+// PutRho compute the rho of an European put option using the Black Schole Formula. Return a float64 and an error object
+func (o *Option) PutRho() (float64, error) {
+	if o.K == 0 {
+		return 0, errors.New("strike price must be greater than 0")
+	}
+	d1 := (math.Log(o.S/o.K) + (o.R+0.5*math.Pow(o.V, 2))*o.T) / (o.V * math.Sqrt(o.T))
+	d2 := d1 - o.V*math.Sqrt(o.T)
+	rho := -o.K * o.T * (math.Exp(-(o.R * o.T))) * standardNormalCDF(-d2)
+	return rho / 100, nil
 }
--- black_scholes_test.go
+++ black_scholes_test.go
@@ -2,11 +2,12 @@ package black_scholes_test
 
 import (
 	"black_scholes"
+	"math"
 	"testing"
 )
 
 func closeEnough(a, b, tolerance float64) bool {
-	return a-b < tolerance
+	return math.Abs(a - b) < tolerance
 }
 
 func TestOptionPricing(t *testing.T) {
@@ -34,4 +35,147 @@ func TestOptionPricing(t *testing.T) {
 	if !closeEnough(gotp, wantp, 0.00001) {
 		t.Errorf("want %v, got %v", wantc, gotc)
 	}
+}
+
+func TestOptionDelta(t *testing.T) {
+	t.Parallel()
+	option := &black_scholes.Option{
+		S: 100.0,
+		K: 100.0,
+		T: 1.0,
+		R: 0.05,
+		V: 0.2,
+	}
+	wantCD := 0.6368306511756191
+	wantPD := -0.3631693488243809
+
+	gotCD, errCD := option.CallDelta()
+	if errCD != nil {
+		t.Fatalf("want no error for invalid input parameter, got %v", errCD)
+	}
+	if !closeEnough(gotCD, wantCD, 0.00001) {
+		t.Errorf("want %v, got %v", wantCD, gotCD)
+	}
+	gotPD, errPD := option.PutDelta()
+	if errPD != nil {
+		t.Fatalf("want no error for invalid input parameter, got %v", errPD)
+	}
+	if !closeEnough(gotPD, wantPD, 0.00001) {
+		t.Errorf("want %v, got %v", wantPD, gotPD)
+	}
+}
+
+func TestOptionGamma(t *testing.T){
+	t.Parallel()
+	option := &black_scholes.Option{
+		S: 100.0,
+		K: 100.0,
+		T: 1.0,
+		R: 0.05,
+		V: 0.2,
+	}
+	wantG := 0.018762017345846895
+
+	gotG, errG := option.Gamma()
+	if errG != nil {
+		t.Fatalf("want no error for invalid input parameter, got %v", errG)
+	}
+	if !closeEnough(gotG, wantG, 0.00001) {
+		t.Errorf("want %v, got %v", wantG, gotG)
+	}
+}
+
+func TestOptionVega(t *testing.T){
+	t.Parallel()
+	option := &black_scholes.Option{
+		S: 100.0,
+		K: 100.0,
+		T: 1.0,
+		R: 0.05,
+		V: 0.2,
+		}
+		wantVeg := 0.3752403469169379
+		gotVeg, errVeg := option.Vega()
+		if errVeg != nil {
+			t.Fatalf("want no error for invalid input parameter, got %v", errVeg)
+		}
+		if !closeEnough(gotVeg, wantVeg, 0.00001) {
+			t.Errorf("want %v, got %v", wantVeg, gotVeg)
+		}
+}
+
+func TestOptionCallTheta(t *testing.T){
+	t.Parallel()
+	option := &black_scholes.Option{
+		S: 100.0,
+		K: 100.0,
+		T: 1.0,
+		R: 0.05,
+		V: 0.2,
+		}
+		wantCT := -0.01757267820941972
+		gotCT, errCT := option.CallTheta()
+		if errCT != nil {
+			t.Fatalf("want no error for invalid input parameter, got %v", errCT)
+		}
+		if !closeEnough(gotCT, wantCT, 0.00001) {
+			t.Errorf("want %v, got %v", wantCT, gotCT)
+		}
+}
+
+func TestOptionPutTheta(t *testing.T){
+	t.Parallel()
+	option := &black_scholes.Option{
+		S: 100.0,
+		K: 100.0,
+		T: 1.0,
+		R: 0.05,
+		V: 0.2,
+		}
+		wantPT := -0.004542138147766099
+		gotPT, errPT := option.PutTheta()
+		if errPT != nil {
+			t.Fatalf("want no error for invalid input parameter, got %v", errPT)
+		}
+		if !closeEnough(gotPT, wantPT, 0.00001) {
+			t.Errorf("want %v, got %v", wantPT, gotPT)
+		}
+}
+
+func TestOption_CallRho(t *testing.T){
+	t.Parallel()
+	option := &black_scholes.Option{
+		S: 100.0,
+		K: 100.0,
+		T: 1.0,
+		R: 0.05,
+		V: 0.2,
+		}
+		wantCR := 0.5323248154537634
+		gotCR, errCR := option.CallRho()
+		if errCR != nil {
+			t.Fatalf("want no error for invalid input parameter, got %v", errCR)
+		}
+		if !closeEnough(gotCR, wantCR, 0.00001) {
+			t.Errorf("want %v, got %v", wantCR, gotCR)
+		}
+}
+
+func TestOption_PutRho(t *testing.T){
+	t.Parallel()
+	option := &black_scholes.Option{
+		S: 100.0,
+		K: 100.0,
+		T: 1.0,
+		R: 0.05,
+		V: 0.2,
+		}
+		wantPR := -0.4189046090469506
+		gotPR, errPR := option.PutRho()
+		if errPR != nil {
+			t.Fatalf("want no error for invalid input parameter, got %v", errPR)
+		}
+		if !closeEnough(gotPR, wantPR, 0.00001) {
+			t.Errorf("want %v, got %v", wantPR, gotPR)
+		}
 }
